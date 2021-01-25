dfb7bb1f141dc6710fd3fd1476b07b2cb783b6fe
dimaefimov <klimmerst@gmail.com>
2021-01-26 00:03:40 +0300

    убрал ненужный print

--- Tasks/g0_bubble_sort.py
+++ Tasks/g0_bubble_sort.py
@@ -11,7 +11,6 @@ def sort(container: List[int]) -> List[int]:
 
     for i in range(len(container) - 1):
         for j in range(len(container) - i - 1):
-            print(container)
             if container[j] > container[j + 1]:
                 container[j], container[j + 1] = container[j + 1], container[j]
 

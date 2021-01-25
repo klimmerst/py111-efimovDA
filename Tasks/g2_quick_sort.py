c6564a6c13a046e5a93910f76b589fafd8dac9b8
dimaefimov <klimmerst@gmail.com>
2021-01-26 02:20:49 +0300

    done

--- Tasks/g2_quick_sort.py
+++ Tasks/g2_quick_sort.py
@@ -8,4 +8,25 @@ def sort(container: List[int]) -> List[int]:
     :param container: container of elements to be sorted
     :return: container sorted in ascending order
     """
-    return container
+
+    if not container:
+        return container
+    
+    if len(container) == 1:
+        return container
+
+    middle = len(container) // 2
+
+    left_cont = []
+    right_cont = []
+    same_cont = []
+
+    for i in container:
+        if i > container[middle]:
+            right_cont.append(i)
+        elif i < container[middle]:
+            left_cont.append(i)
+        elif i == container[middle]:
+            same_cont.append(i)
+
+    return sort(left_cont) + same_cont + sort(right_cont)
